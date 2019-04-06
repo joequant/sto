acfa03e250804c8ea0c86713535c9b972c6edda4
Joseph C Wang <joequant@gmail.com>
2019-04-07 01:11:15 +0800

    update basedev

--- basedev/docker-script.sh
+++ basedev/docker-script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 useradd user
-dnf install -y --allowerasing --best --setopt=install_weak_deps=False --nodocs git npm make gcc-c++
+dnf install -y --allowerasing --best --setopt=install_weak_deps=False --nodocs git npm make gcc-c++ curl cmake llvm --refresh
 npm install -g truffle@v4 yarn ganache-cli
 su user -c '/bin/bash /tmp/docker-script-user.sh'
 
