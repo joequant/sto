2de6b20c3cc162028394255ef52850babaefb9bb
Joseph C Wang <joequant@gmail.com>
2019-04-07 14:51:57 +0800

    add sudo

--- basedev/docker-script.sh
+++ basedev/docker-script.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 useradd user
-dnf install -y --allowerasing --best --setopt=install_weak_deps=False --nodocs git npm make gcc-c++ curl cmake llvm --refresh
+dnf install -y --allowerasing --best --setopt=install_weak_deps=False --nodocs git npm make gcc-c++ curl cmake llvm sudo --refresh
 npm install -g truffle@v4 yarn ganache-cli
 su user -c '/bin/bash /tmp/docker-script-user.sh'
-
+cat <<EOF > /etc/sudoers.d/90user
+user ALL=(ALL) NOPASSWD:ALL
+EOF
