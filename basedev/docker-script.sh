e4c222b90e4590f5f55a78a8e00fc0b83af2f437
Joseph C Wang <joequant@gmail.com>
2019-04-08 11:45:27 +0800

    remove cmake

--- basedev/docker-script.sh
+++ basedev/docker-script.sh
@@ -8,7 +8,7 @@ set -e
 #export GIT_PROXY=http://localhost:8080/
 
 useradd user
-dnf install -y --allowerasing --best --setopt=install_weak_deps=False --nodocs git npm make gcc-c++ curl cmake llvm sudo --refresh
+dnf install -y --allowerasing --best --setopt=install_weak_deps=False --nodocs git npm make gcc-c++ curl llvm sudo --refresh
 npm install -g truffle@v4 yarn modclean
 su user -p -c '/bin/bash /tmp/docker-script-user.sh'
 cat <<EOF > /etc/sudoers.d/90user
