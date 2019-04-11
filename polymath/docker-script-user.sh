a0b859bf7349e297d6f25920daf1c7fd7ce2febb
Joseph C Wang <joequant@gmail.com>
2019-04-12 00:43:39 +0800

    fix up simplerestrictedtoken

--- polymath/docker-script-user.sh
+++ polymath/docker-script-user.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 cd /home/user
 git clone https://github.com/PolymathNetwork/polymath-core.git -b dev-2.2.0
-cd polymath-core
+pushd polymath-core
 yarn
 truffle compile
+popd
+git clone https://github.com/simple-restricted-token/simple-restricted-token.git
+pushd simple-restricted-token
+yarn
+truffle compile
+popd
 
