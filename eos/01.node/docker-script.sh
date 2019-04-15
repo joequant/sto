603ff6123c1e4e382bbf0f77b0dc171853aca63b
Joseph C Wang <joequant@gmail.com>
2019-04-16 03:01:31 +0800

    fix up eos build

--- eos/01.node/docker-script.sh
+++ eos/01.node/docker-script.sh
@@ -16,7 +16,12 @@ if [[ ! -z "$http_proxy" ]] ; then
     yarn config set strict-ssl false
 fi
 
-dnf install -y --allowerasing --best --setopt=install_weak_deps=False --nodocs libedit-devel ncurses-devel
+dnf install -y --allowerasing --best --setopt=install_weak_deps=False --nodocs libedit-devel ncurses-devel git sudo procps-ng which gcc gcc-c++ autoconf automake libtool make \
+	bzip2-devel wget bzip2 compat-openssl10 graphviz doxygen \
+	openssl-devel gmp-devel libstdc++-devel python2 python2-devel python3 python3-devel \
+	libedit ncurses-devel swig libcurl-devel libusb-devel
+
+dnf upgrade -y --setopt=install_weak_deps=False --nodocs --allowerasing --best
 rpm -Uvh \
     http://download-ib01.fedoraproject.org/pub/fedora/linux/releases/28/Everything/x86_64/os/Packages/l/llvm4.0-4.0.1-5.fc28.x86_64.rpm \
     http://download-ib01.fedoraproject.org/pub/fedora/linux/releases/28/Everything/x86_64/os/Packages/l/llvm4.0-libs-4.0.1-5.fc28.x86_64.rpm \
@@ -26,4 +31,4 @@ rpm -Uvh --nodeps http://download-ib01.fedoraproject.org/pub/fedora/linux/releas
 su user -p -c '/bin/bash /tmp/docker-script-user.sh'
 
 npm config delete registry
-cd /home/user/git/sto/eos && echo "master:$(git rev-parse HEAD)" > /etc/eosio-version
+cd /home/user/eos && echo "master:$(git rev-parse HEAD)" > /etc/eosio-version
