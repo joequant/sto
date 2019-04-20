6876f0b95ff3efff459185a2ef25118328cd9955
Joseph C Wang <joequant@gmail.com>
2019-04-20 08:36:44 +0800

    fix comment

--- basedev/docker-script.sh
+++ basedev/docker-script.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 set -e
+: '
 export http_proxy=http://192.168.1.139:3128/
 export https_proxy=http://192.168.1.139:3128/
 export ftp_proxy=http://192.168.1.139:3128/
 export HTTP_PROXY=http://192.168.1.139:3128/
 export PIP_INDEX_URL=http://localhost:3141/root/pypi/+simple/
 export GIT_PROXY=http://localhost:8080/
+'
 
 useradd user
 dnf install -y --allowerasing --best --setopt=install_weak_deps=False --nodocs git npm make gcc-c++ curl sudo patch --refresh
