6ff71d8a3ebc29dca25f3823ab402adbfc9aa268
Joseph C Wang <joequant@gmail.com>
2019-04-20 13:39:47 +0800

    redo proxies

--- polymath-dev/docker-script-user.sh
+++ polymath-dev/docker-script-user.sh
@@ -1,26 +1,31 @@
 #!/bin/bash
 set -e
+set -v
 
 export HOME=/home/user
-
 if [[ ! -z "$http_proxy" ]] ; then
-    npm config set registry http://registry.npmjs.org/
-    npm set strict-ssl false
-    yarn config set registry http://registry.yarnpkg.com/
-    yarn config set strict-ssl false
+    git config --global http.proxy $http_proxy
+    if [[ ! -z "$GIT_PROXY" ]] ; then
+	git config --global url."$GIT_PROXY".insteadOf https://
+    fi
+    git config --global http.sslVerify false
 fi
 
 cd /home/user
 
 git clone https://github.com/PolymathNetwork/polymath-core.git -b dev-3.0.0
 pushd polymath-core
-curl https://github.com/PolymathNetwork/polymath-core/commit/dc50fcb3778f37ab1f6fa580aa1b6fabb362e824.patch | patch -p1
+curl https://github.com/joequant/polymath-core/commit/0032ffc1c3fbf3f3aa08c7daec2f3eff5457ec3d.patch | patch -p1
 cp /tmp/truffle-config.js .
+rm -f yarn.lock
 yarn
 truffle compile
+rm -f yarn.lock
 pushd CLI
+rm -f yarn.lock
 yarn
 truffle compile
+rm -f yarn.lock
 popd
 popd
 
@@ -28,8 +33,13 @@ git clone https://github.com/simple-restricted-token/simple-restricted-token.git
 pushd simple-restricted-token
 cp /tmp/srt/truffle-config.js ./truffle-config.js
 cp /tmp/srt/package.json ./package.json
+rm -f yarn.lock
 yarn
 truffle compile
+rm -f yarn.lock
 popd
-
+git config --unset --global http.proxy || true
+git config --unset --global http.sslVerify || true
+git config --unset --global url."http://127.0.0.1:8080/".insteadOf || true
 npm config delete registry
+
--- polymath-dev/docker-script.sh
+++ polymath-dev/docker-script.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
 set -e
 set -v
-: '
+
+#: '
+#cache with squid
 export http_proxy=http://172.17.0.1:3128/
 export https_proxy=http://172.17.0.1:3128/
 export ftp_proxy=http://172.17.0.1:3128/
 export HTTP_PROXY=http://172.17.0.1:3128/
-export PIP_INDEX_URL=http://localhost:3141/root/pypi/+simple/
-export GIT_PROXY=http://localhost:8080/
-'
-
-if [[ ! -z "$http_proxy" ]] ; then
-    npm config set registry http://registry.npmjs.org/
-    npm set strict-ssl false
-    yarn config set registry http://registry.yarnpkg.com/
-    yarn config set strict-ssl false
-fi
+#cache with devpi-server
+export PIP_INDEX_URL=http://127.0.0.1:3141/root/pypi/+simple/
+#cache with git-cache-http-server
+export GIT_PROXY=http://127.0.0.1:8080/
+#cache with verdacchio
+export NPM_CONFIG_REGISTRY=http://127.0.0.1:4873/
+export YARN_REGISTRY=http://127.0.0.1:4873/
+#'
 
 npm install -g truffle --unsafe-perm
 su user -p -c '/bin/bash /tmp/docker-script-user.sh'
-npm config delete registry
 
