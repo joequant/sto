824d448ef764c8de6edb45a267d8f38b42d2fa5b
Joseph C Wang <joequant@gmail.com>
2019-04-20 08:51:59 +0800

    fix up polymath-dev

--- polymath-dev/docker-script-user.sh
+++ polymath-dev/docker-script-user.sh
@@ -1,16 +1,35 @@
 #!/bin/bash
+set -e
+
 export HOME=/home/user
+
+if [[ ! -z "$http_proxy" ]] ; then
+    npm config set registry http://registry.npmjs.org/
+    npm set strict-ssl false
+    yarn config set registry http://registry.yarnpkg.com/
+    yarn config set strict-ssl false
+fi
+
 cd /home/user
+
 git clone https://github.com/PolymathNetwork/polymath-core.git -b dev-3.0.0
 pushd polymath-core
+curl https://github.com/PolymathNetwork/polymath-core/commit/dc50fcb3778f37ab1f6fa580aa1b6fabb362e824.patch | patch -p1
 cp /tmp/truffle-config.js .
 yarn
 truffle compile
+pushd CLI
+yarn
+truffle compile
 popd
+popd
+
 git clone https://github.com/simple-restricted-token/simple-restricted-token.git
 pushd simple-restricted-token
-cp /tmp/RegulatedToken.sol contracts/token/R-Token
+cp /tmp/srt/truffle-config.js ./truffle-config.js
+cp /tmp/srt/package.json ./package.json
 yarn
 truffle compile
 popd
 
+npm config delete registry
--- polymath-dev/docker-script.sh
+++ polymath-dev/docker-script.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 set -e
 set -v
-#export http_proxy=http://172.17.0.1:3128/
-#export https_proxy=http://172.17.0.1:3128/
-#export ftp_proxy=http://172.17.0.1:3128/
-#export HTTP_PROXY=http://172.17.0.1:3128/
-#export PIP_INDEX_URL=http://localhost:3141/root/pypi/+simple/
-#export GIT_PROXY=http://localhost:8080/
+: '
+export http_proxy=http://172.17.0.1:3128/
+export https_proxy=http://172.17.0.1:3128/
+export ftp_proxy=http://172.17.0.1:3128/
+export HTTP_PROXY=http://172.17.0.1:3128/
+export PIP_INDEX_URL=http://localhost:3141/root/pypi/+simple/
+export GIT_PROXY=http://localhost:8080/
+'
 
 if [[ ! -z "$http_proxy" ]] ; then
     npm config set registry http://registry.npmjs.org/
@@ -14,7 +16,7 @@ if [[ ! -z "$http_proxy" ]] ; then
     yarn config set registry http://registry.yarnpkg.com/
     yarn config set strict-ssl false
 fi
-export USER=root
+
 npm install -g truffle --unsafe-perm
 su user -p -c '/bin/bash /tmp/docker-script-user.sh'
 npm config delete registry
