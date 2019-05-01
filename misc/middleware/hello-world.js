1ecaf764417056060e11d7ed1bbbb135c0745982
Joseph C Wang <joequant@gail.com>
2019-05-01 09:50:45 +0800

    add test middleware

---
+++ misc/middleware/hello-world.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const http = require('http')
+const app = express()
+const port = 3000
+const server= http.createServer(app);
+
+
+app.get('/', (req, res) => res.send('Hello World!'))
+server.listen(port)
+console.log('running listen')
