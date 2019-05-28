d148fbff7aa0df8787590872cfaccb9711db4533
Joseph C Wang <joequant@gmail.com>
2019-05-28 20:47:04 +0800

    create bitcoind

---
+++ bitcoind/build-bitcoind.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e
+. /tmp/proxy.sh
+
+urpmi bitcoind sudo
+
+rpm --erase info-install
+dnf clean all
+rm -f /var/log/*.log
+rm -rf /var/cache/dnf/*
+rm -rf /usr/lib/udev
+rm -rf /usr/lib/.build-id
+rm -rf /code
+
+#remove systemd
+#Prevent systemd from starting unneeded services
+rm -f /usr/etc/systemd/system/*.wants/*
+pushd /usr/lib/systemd
+
+(cd system/sysinit.target.wants/; for i in *; do [ $i == systemd-tmpfiles-setup.service ] || rm -f $i; done)
+rm -f system/multi-user.target.wants/*
+rm -f system/local-fs.target.wants/*
+rm -f system/sockets.target.wants/*udev*
+rm -f system/sockets.target.wants/*initctl*
+rm -f system/basic.target.wants/*
+rm -f system/anaconda.target.wants/*
+rm -f *udevd* *networkd* *machined* *coredump*
+popd
