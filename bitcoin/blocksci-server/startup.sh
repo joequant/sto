861f147a5b8b1d0a799bd748af335e4da7b7f487
Joseph C Wang <joequant@gmail.com>
2019-05-30 18:49:51 +0800

    delay startup

--- bitcoin/blocksci-server/startup.sh
+++ bitcoin/blocksci-server/startup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 while true; do
     echo "Updating"
-    blocksci_parser /root/blocksci_config update
-    sleep 60
+    blocksci_parser /root/blocksci_config update >> /root/update.log
+    sleep 120
 done
