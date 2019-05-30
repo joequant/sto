4b2cba0daf777b938c2badfb88f5a2c94d0b0266
Joseph C Wang <joequant@gmail.com>
2019-05-30 23:05:23 +0800

    fix rpycd

--- bitcoin/blocksci-server/config-blocksci.sh
+++ bitcoin/blocksci-server/config-blocksci.sh
@@ -4,7 +4,6 @@ pip3 install --upgrade rpyc python-daemon lockfile
 mkdir /var/lib/blocksci
 mkdir /var/lib/bitcoin
 cd /tmp
-cp startup.sh update-blocksci.sh rpycd.py rpycd.conf /root
+cp startup.sh rpycd.py rpycd.conf /root
 chmod a+x /root/startup.sh
-chmod a+x /root/update-blocksci.sh
 /usr/bin/blocksci_parser /root/blocksci_config generate-config bitcoin /var/lib/blocksci --disk /var/lib/bitcoin
--- bitcoin/blocksci-server/startup.sh
+++ bitcoin/blocksci-server/startup.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 cd /root
-./update-blocksci.sh &
-python3 ./rpycd.py
-
-
-
+rm -f ./rpycd.pid
+python3 ./rpycd.py &
+while true; do
+    echo "Updating"
+    blocksci_parser /root/blocksci_config update || true
+    sleep 120
+done
