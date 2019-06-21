61b90d58bd8ff769f4c31f523706bb077a523ef9
Joseph C Wang <joequant@gmail.com>
2019-06-21 21:24:23 +0000

    add graphviz

--- bitcoin/blocksci-server/config-blocksci.sh
+++ bitcoin/blocksci-server/config-blocksci.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 pip3 install --upgrade rpyc python-daemon lockfile Flask gevent bitcoin-etl
-pip3 install --upgrade multiprocess psutil jupyterlab pycrypto matplotlib pandas dateparser
+pip3 install --upgrade multiprocess psutil jupyterlab pycrypto matplotlib pandas dateparser graphviz
 
 add-apt-repository -y ppa:bitcoin/bitcoin
 apt-get update -y 
@@ -9,6 +9,6 @@ apt-get install -y bitcoind
 mkdir /var/lib/blocksci
 mkdir /root/apps
 cd /tmp
-cp startup.sh rpycd.py rpycd.conf app-server.pyi hello-world.py /root/apps
+cp startup.sh rpycd.py rpycd.conf app-server.py hello-world.py /root/apps
 chmod a+x /root/apps/startup.sh
 /usr/bin/blocksci_parser /root/apps/blocksci_config generate-config bitcoin /var/lib/blocksci --disk /var/lib/bitcoin -m -6
