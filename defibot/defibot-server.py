fc2bcdd8f94b12c19ac1bd064547f711151831dd
Joseph C Wang <joequant@gmail.com>
2020-12-19 19:47:24 +0800

    add graphql server

--- defibot/defibot-server.py
+++ defibot/defibot-server.py
@@ -17,5 +17,17 @@ def hello_world():
 def graphql_playground():
     return PLAYGROUND_HTML, 200
 
+@app.route("/graphql", methods=["POST"])
+def graphql_server():
+    data = request.get_json()
+    success, result = graphql_sync(
+        schema,
+        data,
+        context_value=request,
+        debug=app.debug
+    )
+    status_code = 200 if success else 400
+    return jsonify(result), status_code
+
 if __name__ == '__main__':
     app.run()
---
+++ defibot/model.py
@@ -0,0 +1,5 @@
+from ariadne import QueryType
+query = QueryType()
+@query.field("hello")
+def resolve_hello(_, info):
+    return "Hi there"
