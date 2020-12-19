8bf62802492c9e2aa0578f120805358d5e400913
Joseph C Wang <joequant@gmail.com>
2020-12-20 01:14:50 +0800

    add eventloop

--- defibot/test-eventloop.py
+++ defibot/test-eventloop.py
@@ -22,7 +22,6 @@ def main():
     try:
         loop.run_until_complete(
             asyncio.gather(
-                log_loop(block_filter, 2),
                 log_loop(tx_filter, 2)))
     finally:
         loop.close()
