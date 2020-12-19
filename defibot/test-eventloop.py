fcfaa0a38fb8a431b3ad1d673e63b48e194372fa
Joseph C Wang <joequant@gmail.com>
2020-12-20 01:13:10 +0800

    add eventloop

---
+++ defibot/test-eventloop.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+from defibotlocal import DefibotLocal
+import asyncio
+
+dfbl = DefibotLocal()
+w3 = dfbl.web3()
+
+def handle_event(event):
+    print(event)
+    # and whatever
+
+async def log_loop(event_filter, poll_interval):
+    while True:
+        for event in event_filter.get_new_entries():
+            handle_event(event)
+        await asyncio.sleep(poll_interval)
+
+def main():
+    block_filter = w3.eth.filter('latest')
+    tx_filter = w3.eth.filter('pending')
+    loop = asyncio.get_event_loop()
+    try:
+        loop.run_until_complete(
+            asyncio.gather(
+                log_loop(block_filter, 2),
+                log_loop(tx_filter, 2)))
+    finally:
+        loop.close()
+
+if __name__ == '__main__':
+    main()
--- defibot/test-pending.py
+++ defibot/test-pending.py
@@ -3,4 +3,7 @@ import defibotlocal
 
 dfb = defibotlocal.DefibotLocal()
 print(dfb.test_pending())
+web3 = dfb.web3()
+new_transaction_filter = web3.eth.filter('pending')
+print(new_transaction_filter.get_new_entries())
 
