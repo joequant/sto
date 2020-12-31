6705aa8d0f3459bd088ab3fef15d7fd3f2e6ceef
Joseph C Wang <joequant@gmail.com>
2020-12-31 08:34:55 +0800

    add tests

--- defibot/test-uniswap.py
+++ defibot/test-uniswap.py
@@ -2,7 +2,10 @@
 import defibotlocal
 
 dfb = defibotlocal.DefibotLocal()
+print(dfb.eth_price())
 print(dfb.test_uniswap())
 u = dfb.uniswap_write()
 print(u.get_weth_address())
-
+print(dfb.get_balance())
+#SFI
+print(dfb.get_balance('0xb753428af26e81097e7fd17f40c88aaa3e04902c'))
---
+++ defibot/tests/test_backtest.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+from defibot import Defibot
+
+def test_backtest():
+    dfb = Defibot()
+    dfb.backtest(11517197,11517197)
