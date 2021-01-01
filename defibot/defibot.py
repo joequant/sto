3174d2d23364d98a693dca8ff2a5915f4c942043
Joseph C Wang <joequant@gmail.com>
2021-01-02 06:49:05 +0800

    add hints

--- defibot/defibot.py
+++ defibot/defibot.py
@@ -5,20 +5,28 @@ import logging
 import asyncio
 import datetime
 import requests
+from typing import Optional, Union, Dict
 from uniswap.uniswap import UniswapV2Client
 import web3
 from web3 import Web3
-import json5
+import json5 # type: ignore
 from dictcache import DictCache
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.WARNING)
 logger.addHandler(logging.StreamHandler(sys.stdout))
 
+numeric = Union[int, float]
+identifier = Union[int, str]
+
 async def txn_loop(defibot, event_filter, poll_interval):
     while True:
         for event in event_filter.get_new_entries():
-            defibot.handle_txn(event)
+            try:
+                txn = defibot.web3().eth.getTransaction(event)
+                defibot.handle_txn(txn)
+            except web3.exceptions.TransactionNotFound:
+                pass
         await asyncio.sleep(poll_interval)
 
 async def block_loop(defibot, event_filter, poll_interval):
@@ -28,7 +36,7 @@ async def block_loop(defibot, event_filter, poll_interval):
             defibot.handle_block(block)
         await asyncio.sleep(poll_interval)
 
-def match_nocase(a, b):
+def match_nocase(a: str, b: str) -> bool:
     return a.lower() == b.lower()
 
 def normalize_decimal(a, b):
@@ -42,17 +50,17 @@ ETH_DECIMALS=18
 class Defibot:
     def __init__(self,
                  router=["0x7a250d5630b4cf539739df2c5dacb4c659f2488d"],
-                 name=None):
+                 name: Optional[str]=None):
         script_dir = os.path.dirname(os.path.realpath(__file__))
         self.name = type(self).__name__ if name is None else name
         with open(os.path.join(script_dir, 'config.json')) as f:
             self._config = json5.load(f)
-        self._uniswap = None
+        self._uniswap: Optional[UniswapV2Client] = None
         self.router = router
-        self._uniswap_write = None
-        self._web3 = None
-        self._web3_write = None
-        self._abi_cache = {}
+        self._uniswap_write: Optional[UniswapV2Client] = None
+        self._web3: Optional[Web3] = None
+        self._web3_write: Optional[Web3] = None
+        self._abi_cache: Dict[str, str] = {}
         self.wait_async = 1
         self.token_cache = DictCache("token-" + self.name)
         self.pair_cache = DictCache("pair-" + self.name)
@@ -70,15 +78,17 @@ class Defibot:
                 self.uniswap().get_weth_address()
         return self._weth_address
 
-    def web3(self):
+    def web3(self) -> Web3:
         if self._web3 is None:
             provider = self.config('provider')
             if "http:" in provider or "https:" in provider:
                 self._web3 = Web3(Web3.HTTPProvider(provider))
             elif "ws:" in provider or "wss:" in provider:
                 self._web3 = Web3(Web3.WebsocketProvider(provider))
+            else:
+                raise NotImplementedError
         return self._web3
-    def web3_write(self):
+    def web3_write(self) -> Web3:
         if 'provider_write' not in self._config:
             return self.web3()
         if self._web3_write is None:
@@ -87,8 +97,10 @@ class Defibot:
                 self._web3_write = Web3(Web3.HTTPProvider(provider))
             elif "ws:" in provider or "wss:" in provider:
                 self._web3_write = Web3(Web3.WebsocketProvider(provider))
+            else:
+                raise NotImplementedError
         return self._web3_write
-    def uniswap(self):
+    def uniswap(self) -> UniswapV2Client:
         if self._uniswap is None:
             if 'provider_archive' in self._config:
                 provider = self.config('provider_archive')
@@ -98,7 +110,7 @@ class Defibot:
                                             self.config('private_key'),
                                             provider=provider)
         return self._uniswap
-    def uniswap_write(self):
+    def uniswap_write(self) -> UniswapV2Client:
         if 'provider_write' not in self._config:
             return self.uniswap()
         if self._uniswap_write is None:
@@ -108,16 +120,13 @@ class Defibot:
         return self._uniswap_write
     def pending_txns(self):
         return None
-    def handle_txn(self, event, block_identifier='latest'):
-        try:
-            txn = self.web3().eth.getTransaction(event.hex())
-            if self.router is None or \
-               (txn is not None and txn['to'] is not None \
-                and txn['to'].lower() in self.router):
-                self.process_txn(txn, block_identifier)
-        except web3.exceptions.TransactionNotFound:
-            pass
-        # and whatever
+
+    def handle_txn(self, txn, block_identifier='latest'):
+        if self.router is None or \
+           (txn is not None and txn['to'] is not None \
+            and txn['to'].lower() in self.router):
+            self.process_txn(txn, block_identifier)
+
     def handle_block(self, event):
         print("block - ", event)
     def process_txn(self, txn, block_identifier='latest'):
@@ -126,7 +135,7 @@ class Defibot:
         response = requests.get('https://www.gasnow.org/api/v3/gas/price?utm_source=defibot')
         response.raise_for_status()
         return response.json()
-    def get_abi(self, contract):
+    def get_abi(self, contract: str):
         if contract not in self._abi_cache:
             response = \
                 requests.get("https://api.etherscan.io/api?module=contract&action=getabi&address={}".format(contract))
@@ -154,7 +163,7 @@ class Defibot:
     def data(self):
         return []
 
-    def query(self, endpoint, query):
+    def query(self, endpoint: str, query: str):
         request = requests.post(
             'https://api.thegraph.com/subgraphs/name/{}'.format(endpoint),
             json={'query': query}
@@ -162,7 +171,7 @@ class Defibot:
         request.raise_for_status()
         return request.json()
 
-    def token_info_graphql(self, token):
+    def token_info_graphql(self, token: str):
         if token not in self.token_cache:
             query = """{
   token(id:"%s") {
@@ -182,9 +191,9 @@ class Defibot:
             retval = retval['data']['token']
             self.token_cache[token] = retval
         return self.token_cache[token]
-    def token_info(self, token):
+    def token_info(self, token: str):
         return self.token_info_graphql(token.lower())
-    def pair_info(self, token0, token1):
+    def pair_info(self, token0: str, token1: str):
         key = token0.lower() + token1.lower()
         if key not in self.pair_cache:
             self.pair_cache[key] = \
@@ -192,7 +201,7 @@ class Defibot:
         return self.pair_cache[key]
     def now(self):
         return datetime.datetime.now().timestamp()
-    def gas_use(self, d):
+    def gas_use(self, d: Dict):
         if not isinstance(d, dict):
             return None
         if 'func' not in d:
@@ -207,22 +216,25 @@ class Defibot:
                 'swapExactTokensForTokens']:
             return 120000
 
-    def backtest(self, block_start='latest',
-                 block_finish=None):
-        if block_start == 'latest':
-            block_start = self.web3().eth.blockNumber
-        if block_finish is None:
+    def backtest(self, block_start_id: identifier='latest',
+                 block_finish_id: Optional[identifier]=None):
+        block_start = self.web3().eth.blockNumber\
+            if block_start_id == 'latest' else int(block_start_id)
+        if block_finish_id is None:
             block_finish = block_start
-        elif block_finish == 'latest':
+        elif block_finish_id == 'latest':
             block_finish = self.web3().eth.blockNumber
+        else:
+            block_finish = int(block_finish_id)
         for i in range(block_start, block_finish+1):
             block = self.web3().eth.getBlock(i)
             for t in block['transactions']:
                 logger.debug("txn - %s", t.hex())
-                self.handle_txn(t, i-1)
+                self.handle_txn(self.web3().eth.getTransaction(t), i-1)
             self.handle_block(block)
 
-    def get_balance(self, contract=None, block_identifier="latest"):
+    def get_balance(self, contract:Optional[str]=None,
+                    block_identifier: identifier="latest"):
         if contract == self.get_weth_address() or \
            contract is None:
             return self.web3().eth.getBalance(
@@ -235,7 +247,8 @@ class Defibot:
                 abi=self.uniswap().ERC20_ABI
             )
             return erc20_contract.functions.balanceOf(self.config('address')).call()
-    def eth_price(self, block_identifier="latest"):
+
+    def eth_price(self, block_identifier: identifier="latest"):
         usdt = '0xdAC17F958D2ee523a2206206994597C13D831ec7'
         usdt_decimals = int(self.token_info(usdt)['decimals'])
         reserve = self.get_reserves(self.get_weth_address(),
@@ -245,7 +258,8 @@ class Defibot:
             reserve[1]/reserve[0],
             usdt_decimals - ETH_DECIMALS
         )
-    def token_price(self, token, block_identifier="latest"):
+    def token_price(self, token :str,
+                    block_identifier: identifier="latest"):
         if match_nocase(token, self.get_weth_address()):
             return self.eth_price(block_identifier)
         token_decimals = int(self.token_info(token)['decimals'])
@@ -261,14 +275,17 @@ class Defibot:
         )
         return token_to_eth * self.eth_price(block_identifier)
 
-    def get_reserves(self, token_a, token_b, block_identifier):
+    def get_reserves(self,
+                     token_a: str, token_b: str,
+                     block_identifier: identifier):
         if block_identifier == "latest":
             u = self.uniswap()
             return u.get_reserves(token_a,
                                   token_b,
                                   block_identifier)
         key = "%s%s%d" % (token_a.lower(),
-                          token_b.lower(), block_identifier)
+                          token_b.lower(),
+                          int(block_identifier))
         if key not in self.reserves_cache:
             u = self.uniswap()
             self.reserves_cache[key] = \
